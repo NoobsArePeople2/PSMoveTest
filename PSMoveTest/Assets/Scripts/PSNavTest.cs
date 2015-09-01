8afc8ee1af9bb193060986bb7a271b929ab6381d
Sean Monahan <sean@seanmonahan.org>
2015-09-01 08:57:53 -0700

    Add script for testing input.

---
+++ PSMoveTest/Assets/Scripts/PSNavTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PSNavTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (Input.GetJoystickNames ().Length + " joysticks connected.");
+		foreach (string name in Input.GetJoystickNames()) {
+			Debug.Log ("JS: " + name);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetAxis ("Horizontal") != 0.0f) {
+			Debug.Log ("X: " + Input.GetAxis ("Horizontal"));
+		}
+
+		if (Input.GetAxis ("Vertical") != 0.0f) {
+			Debug.Log ("Y: " + Input.GetAxis ("Vertical"));
+		}
+
+	}
+}
